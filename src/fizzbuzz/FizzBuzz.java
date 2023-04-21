f539aaecb548dbd634488e4857645e0d21e19251
JulMig <julie.mignotte8@gmail.com>
2023-04-21 14:12:54 +0200

    fizzbuzz(1)

--- src/fizzbuzz/FizzBuzz.java
+++ src/fizzbuzz/FizzBuzz.java
@@ -1,5 +1,12 @@
 package fizzbuzz;
 
+
 public class FizzBuzz {
+	
+	public static final String UN = "1";
+
+	public String fizzBuzz(int i) {
+		return UN;
+	}
 
 }
--- test/fizzbuzz/FizzBuzzTest.java
+++ test/fizzbuzz/FizzBuzzTest.java
@@ -7,13 +7,16 @@ import org.junit.jupiter.api.Test;
 
 class FizzBuzzTest {
 
+	FizzBuzz fb;
+	
 	@BeforeEach
 	void setUp() throws Exception {
+		fb = new FizzBuzz();
 	}
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void premier_test() {
+		assertEquals("1", fb.fizzBuzz(1));
 	}
 
 }
